e310cb608efb3a3b357d66f2200944a730f5dae5
Mateusz Gostański <mg@grixu.dev>
2021-02-25 14:38:46 +0100

    Updated WarehouseDataFactory

--- src/Warehouse/Factories/WarehouseDataFactory.php
+++ src/Warehouse/Factories/WarehouseDataFactory.php
@@ -3,7 +3,7 @@
 namespace Grixu\SociusModels\Warehouse\Factories;
 
 use Grixu\SociusModels\Warehouse\DataTransferObjects\WarehouseData;
-use Grixu\SociusModels\Warehouse\Enums\WarehouseLockEnum;
+use Grixu\SociusModels\Warehouse\Enums\WarehouseTypeEnum;
 use Grixu\DataFactories\Factory;
 
 class WarehouseDataFactory extends Factory
@@ -20,12 +20,12 @@ class WarehouseDataFactory extends Factory
             [
                 'name' => 'Testowy magazyn',
                 'desc' => 'Testowy opis',
-                'internal' => (bool) rand(0,1),
                 'country' => 'PL',
-                'stockCounting' => (bool) rand(0,1),
-                'stockCountingDate' => now()->subDay(),
-                'lastModification' => now()->subWeek(),
-                'locked' => WarehouseLockEnum::UNLOCKED(),
+                'street' => 'Ulica',
+                'city' => 'Miasto',
+                'postCode' => '00-000',
+                'type' => WarehouseTypeEnum::DEPOSIT(),
+                'locked' => (bool) rand(0,1),
                 'syncTs' => now(),
                 'updatedAt' => now(),
                 'xlId' => 1,
