02faf6dc928cf24d82ed2aa959a600f21e66bb50
Mateusz Gostański <mg@grixu.dev>
2021-02-26 09:38:55 +0100

    Created test case for DTO factory for OrderElementData

---
+++ tests/Order/OrderElementDataFactoryTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Grixu\SociusModels\Tests\Order;
+
+use Grixu\SociusModels\Order\Factories\OrderElementDataFactory;
+use Grixu\SociusModels\Tests\Helpers\DataFactoryTestCase;
+
+class OrderElementDataFactoryTest extends DataFactoryTestCase
+{
+    protected string $factory = OrderElementDataFactory::class;
+}
