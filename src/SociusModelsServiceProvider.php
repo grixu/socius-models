4578ab25836f1062b3707485b1d080a49042387b
Mateusz Gostański <mg@grixu.dev>
2021-02-26 10:16:38 +0100

    Updated CHANGELOG.md

--- src/SociusModelsServiceProvider.php
+++ src/SociusModelsServiceProvider.php
@@ -23,6 +23,7 @@ class SociusModelsServiceProvider extends ServiceProvider
                     __DIR__ . '/../migrations/operator' => database_path('migrations'),
                     __DIR__ . '/../migrations/customer' => database_path('migrations'),
                     __DIR__ . '/../migrations/warehouse' => database_path('migrations'),
+                    __DIR__ . '/../migrations/order' => database_path('migrations'),
                 ],
                 'socius-migrations'
             );
@@ -60,6 +61,13 @@ class SociusModelsServiceProvider extends ServiceProvider
                     __DIR__ . '/../migrations/warehouse' => database_path('migrations'),
                 ],
                 'socius-migrations-warehouse'
+            );
+
+            $this->publishes(
+                [
+                    __DIR__ . '/../migrations/order' => database_path('migrations'),
+                ],
+                'socius-migrations-order',
             );
         }
     }
