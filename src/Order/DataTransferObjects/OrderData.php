17e526a6891d41814b6399db95d46b7e51d34b01
Mateusz Gostański <mg@grixu.dev>
2021-03-03 09:40:54 +0100

    Removed FKs from OrderData & OrderElementData

--- src/Order/DataTransferObjects/OrderData.php
+++ src/Order/DataTransferObjects/OrderData.php
@@ -4,18 +4,14 @@ namespace Grixu\SociusModels\Order\DataTransferObjects;
 
 use Grixu\RelationshipDataTransferObject\RelationshipDataTransferObject;
 use Grixu\SociusModels\Order\Enums\ReceiveStatusEnum;
-use Grixu\SociusModels\Order\Enums\SendingStatusEnum;
 use Illuminate\Support\Carbon;
 
 class OrderData extends RelationshipDataTransferObject
 {
     public int $xlId;
-    public string $orderNumber;
+    public ?string $orderNumber;
     public ReceiveStatusEnum $receiveStatus;
     public ?Carbon $receiveCreatedAt;
     public ?Carbon $receiveUpdatedAt;
-    public SendingStatusEnum $sendingStatus;
-    public ?Carbon $sendingCreatedAt;
-    public ?Carbon $sendingUpdatedAt;
     public ?string $receivedDetailedStatus;
 }
--- src/Order/DataTransferObjects/OrderElementData.php
+++ src/Order/DataTransferObjects/OrderElementData.php
@@ -8,12 +8,8 @@ use Illuminate\Support\Carbon;
 class OrderElementData extends RelationshipDataTransferObject
 {
     public int $xlId;
-    public int $orderId;
-    public int $productId;
     public string $productIndex;
-    public int $warehouseId;
     public float $amount;
-    public Carbon $sentAt;
     public ?Carbon $receivedAt;
     public ?string $receivedDetailedStatus;
 }
