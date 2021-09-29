e87e08291c702c3cb6700a59ea4f15609357d883
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-09-29 14:37:39 +0200

    test: added tests for 100% coverage
    Signed-off-by: Mateusz Gostański <mateusz.gostanski@gmail.com>

--- tests/Warehouse/WarehouseDataFactoryTest.php
+++ tests/Warehouse/WarehouseDataFactoryTest.php
@@ -2,10 +2,22 @@
 
 namespace Grixu\SociusModels\Tests\Warehouse;
 
+use Grixu\SociusDto\SociusDto;
 use Grixu\SociusModels\Tests\Helpers\DataFactoryTestCase;
 use Grixu\SociusModels\Warehouse\Factories\WarehouseDataFactory;
 
 class WarehouseDataFactoryTest extends DataFactoryTestCase
 {
     protected string $factory = WarehouseDataFactory::class;
+
+    /** @test */
+    public function caster_check()
+    {
+        $obj = $this->factory::new()->create([
+        'type' => 'Depozyt',
+    ]);
+
+        $this->assertNotEmpty($obj);
+        $this->assertInstanceOf(SociusDto::class, $obj);
+    }
 }
